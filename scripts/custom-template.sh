7ea05f9fb3a451cfc5cfdb4976eb995c574036e1
Marcus Girolneto <marcus.girolneto@gmail.com>
2023-07-07 10:54:56 -0300

    Updating Euclid to support Data API

--- scripts/custom-template.sh
+++ scripts/custom-template.sh
@@ -1,51 +1,43 @@
 function create_template() {
-  cd source/$1
-  if [ -d "$2" ]; then
-    echo "Template already exists!"
-  else
-    echo "Generating template $1 ..."
-
-    echo "Template version: $TEMPLATE_VERSION"
-    echo "Template version is tag or branch: $TEMPLATE_VERSION_IS_TAG_OR_BRANCH"
-    echo "Version name to use: $VERSION_NAME_TO_USE"
-
-    if [ "$TEMPLATE_VERSION_IS_TAG_OR_BRANCH" == "tag" ]; then
-      g8 Constellation-Labs/currency --tag $TEMPLATE_VERSION --name="$2" --tessellation_version="$VERSION_NAME_TO_USE"
+    cd source/$1
+    if [ -d "$FRAMEWORK_NAME" ]; then
+        echo "Template already exists!"
     else
-      g8 Constellation-Labs/currency --branch $TEMPLATE_VERSION --name="$2" --tessellation_version="$VERSION_NAME_TO_USE"
+        echo "Generating template $FRAMEWORK_NAME ..."
+        
+        echo "Template version: $TEMPLATE_VERSION"
+        echo "Template version is tag or branch: $TEMPLATE_VERSION_IS_TAG_OR_BRANCH"
+        
+        if [ "$TEMPLATE_VERSION_IS_TAG_OR_BRANCH" == "tag" ]; then
+            if [[ " ${FRAMEWORK_MODULES[*]} " =~ "data" ]]; then
+                g8 Constellation-Labs/currency --tag $TEMPLATE_VERSION --name="$FRAMEWORK_NAME" --tessellation_version="$TESSELLATION_VERSION" --include_data_l1="yes"
+            else
+                g8 Constellation-Labs/currency --tag $TEMPLATE_VERSION --name="$FRAMEWORK_NAME" --tessellation_version="$TESSELLATION_VERSION"
+            fi
+        else
+            if [[ " ${FRAMEWORK_MODULES[*]} " =~ "data" ]]; then
+                g8 Constellation-Labs/currency --branch $TEMPLATE_VERSION --name="$FRAMEWORK_NAME" --tessellation_version="$TESSELLATION_VERSION" --include_data_l1="yes"
+            else
+                g8 Constellation-Labs/currency --branch $TEMPLATE_VERSION --name="$FRAMEWORK_NAME" --tessellation_version="$TESSELLATION_VERSION"
+            fi
+        fi
+        
     fi
-
-  fi
-  cd ../../
+    cd ../../
 }
 
 function check_if_project_name_is_set() {
-  if [[ -z "$PROJECT_NAME_CURRENCY" ]]; then
-    echo "You should provide the PROJECT_NAME_CURRENCY on hydra.cfg file"
-    exit 1
-  fi
-  if [[ " ${DOCKER_CONTAINERS[*]} " =~ "metagraph-l1-data" ]]; then
-    if [[ -z "$PROJECT_NAME_DATA" ]]; then
-      echo "metagraph-l1-data provided"
-      echo "You should provide the PROJECT_NAME_DATA on hydra.cfg file"
-      exit 1
+    if [[ -z "$FRAMEWORK_NAME" ]]; then
+        echo "You should provide the FRAMEWORK_NAME on hydra.cfg file"
+        exit 1
     fi
-  fi
-
 }
 
 function check_if_project_directory_exists() {
-  cd ../source/
-  if [ ! -d "project/$PROJECT_NAME_CURRENCY" ]; then
-    echo "You must install a framework before building. Run hydra install first"
-    exit 1
-  fi
-  if [[ " ${DOCKER_CONTAINERS[*]} " =~ "metagraph-l1-data" ]]; then
-    if [ ! -d "project/$PROJECT_NAME_DATA" ]; then
-      echo "metagraph-l1-data provided"
-      echo "You must install a framework to metagraph-data before building. Run hydra install first"
-      exit 1
+    cd ../source/
+    if [ ! -d "project/$FRAMEWORK_NAME" ]; then
+        echo "You must install a framework before building. Run hydra install first"
+        exit 1
     fi
-  fi
-  cd ../scripts/
+    cd ../scripts/
 }
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+function fill_env_variables_from_json_config_file() {
+  export GITHUB_TOKEN=$(jq -r .github_token hydra.cfg.json)
+  export METAGRAPH_ID=$(jq -r .metagraph_id hydra.cfg.json)
+  export TESSELLATION_VERSION=$(jq -r .tessellation_version hydra.cfg.json)
+  export TEMPLATE_VERSION=$(jq -r .framework.version hydra.cfg.json)
+  export TEMPLATE_VERSION_IS_TAG_OR_BRANCH=$(jq -r .framework.ref_type hydra.cfg.json)
+  export FRAMEWORK_NAME=$(jq -r .framework.name hydra.cfg.json)
+  export FRAMEWORK_MODULES=$(jq -r .framework.modules hydra.cfg.json)
+  export P12_GENESIS_FILE_NAME=$(jq -r .p12_files.genesis.file_name hydra.cfg.json)
+  export P12_GENESIS_FILE_KEY_ALIAS=$(jq -r .p12_files.genesis.alias hydra.cfg.json)
+  export P12_GENESIS_FILE_PASSWORD=$(jq -r .p12_files.genesis.password hydra.cfg.json)
+  export P12_NODE_2_FILE_NAME=$(jq -r .p12_files.validators[0].file_name hydra.cfg.json)
+  export P12_NODE_2_FILE_KEY_ALIAS=$(jq -r .p12_files.validators[0].alias hydra.cfg.json)
+  export P12_NODE_2_FILE_PASSWORD=$(jq -r .p12_files.validators[0].password hydra.cfg.json)
+  export P12_NODE_3_FILE_NAME=$(jq -r .p12_files.validators[1].file_name hydra.cfg.json)
+  export P12_NODE_3_FILE_KEY_ALIAS=$(jq -r .p12_files.validators[1].alias hydra.cfg.json)
+  export P12_NODE_3_FILE_PASSWORD=$(jq -r .p12_files.validators[1].password hydra.cfg.json)
+  
+  containers_array=$(jq -r .docker.default_containers hydra.cfg.json)
+  export DOCKER_CONTAINERS=( $(echo $containers_array | sed -e 's/\[//g' -e 's/\"//g' -e 's/\]//g' -e 's/\,/ /g') )
+}
+
 function check_if_github_token_is_valid() {
     if curl -s -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/user | grep -q "Bad credentials"; then
         echo "Invalid GITHUB_TOKEN"
@@ -26,20 +48,19 @@ function get_metagraph_id_from_genesis_currency_l0() {
         METAGRAPH_ID=$(docker logs l0-currency-1 -n 1000 2>&1 | grep -o "Address from genesis data is .*" | grep -o "DAG.*")
         if [[ -z "$METAGRAPH_ID" ]]; then
             if [ $i -eq 10 ]; then
-                echo "Could not find the METAGRAPH_ID, check the currency L0 node 1 logs"
+                echo "Could not find the metagraph_id, check the currency L0 node 1 logs"
                 exit 1
             fi
-            echo "METAGRAPH_ID not found trying again in 30s"
+            echo "metagraph_id not found trying again in 30s"
             sleep 30
         else
             cd ../../../
             echo "METAGRAPH_ID found: $METAGRAPH_ID"
-            echo "Filling the hydra.cfg file"
-            echo "$(sed '/METAGRAPH_ID=.*/d' hydra.cfg)" > hydra.cfg
-            echo "METAGRAPH_ID=$METAGRAPH_ID" >> hydra.cfg
+            echo "Filling the hydra.cfg.json file"
+            contents="$(jq --arg METAGRAPH_ID "$METAGRAPH_ID" '.metagraph_id = $METAGRAPH_ID' hydra.cfg.json)" && \
+            echo -E "${contents}" > hydra.cfg.json
             
-            set -o allexport
-            source hydra.cfg set
+            fill_env_variables_from_json_config_file
             
             cd infra/docker/metagraph-l0-genesis
             break
