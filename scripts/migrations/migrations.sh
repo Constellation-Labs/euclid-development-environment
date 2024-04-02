89860b0036eb33da28f8c16aa6a95d3ccc446a88
Marcus Girolneto <marcus.girolneto@gmail.com>
2024-04-02 17:48:09 -0300

    Adding behavior to always update version field

--- scripts/migrations/migrations.sh
+++ scripts/migrations/migrations.sh
@@ -1,25 +1,30 @@
 function run_migrations() {
   echo "Starting migrations..."
   echo
-  local VERSION=$(jq -r '.version // empty' "$ROOT_PATH/euclid.json")
+
+  euclid_field_version=$(jq -r '.version // empty' "$ROOT_PATH/euclid.json")
+  VERSION=$1
+
   cd $INFRA_PATH
-  
+
   if [ ! -d "euclid-development-environment" ]; then
     git clone https://github.com/Constellation-Labs/euclid-development-environment.git
   fi
-  
+
   cp $ROOT_PATH/euclid.json $ROOT_PATH/old-euclid.json
 
-  if [ -z "$VERSION" ]; then
+  if [ -z "$euclid_field_version" ]; then
     echo "Running migration v0.9.0"
     cd $SCRIPTS_PATH
     source ./migrations/v0.9.0.sh
+    VERSION="0.9.0"
     migrate_v_0_9_0
   fi
 
   rm -r -f $INFRA_PATH/euclid-development-environment
   rm -f $ROOT_PATH/old-euclid.json
-
+  contents="$(jq --arg VERSION "$VERSION" ".version = \"$VERSION\"" $ROOT_PATH/euclid.json)" &&
+    echo -E "${contents}" >$ROOT_PATH/euclid.json
   echo "migrations finished..."
   echo
 }
