5cdf15d8d3fd9cd891b18723b35c724b3d3b7c28
Marcus Girolneto <marcus.girolneto@gmail.com>
2024-04-04 11:56:52 -0300

    initial bug fixes

--- scripts/hydra-operations/install.sh
+++ scripts/hydra-operations/install.sh
@@ -6,12 +6,6 @@ function install_project() {
   echo_white "Installing Framework..."
   create_template project
 
-  chmod -R a+rwx $INFRA_PATH/docker/monitoring
-  chmod -R a+rwx $INFRA_PATH/docker/monitoring/grafana/storage/
-  chmod -R a+rwx $INFRA_PATH/docker/monitoring/grafana/dashboards/
-  chmod -R a+rwx $INFRA_PATH/docker/monitoring/grafana/datasources/
-  chmod -R a+rwx $INFRA_PATH/docker/monitoring/prometheus/
-
   cd $ROOT_PATH
   if [ -d ".git" ]; then
     chmod -R +w .git
