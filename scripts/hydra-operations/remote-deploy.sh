b0b7d35bf523f390ff32ee7e99562749d833d296
Marcus Girolneto <marcus.girolneto@gmail.com>
2024-04-04 14:12:56 -0300

    Updating from deploy_only_metagraph_jars to force_genesis

--- scripts/hydra-operations/remote-deploy.sh
+++ scripts/hydra-operations/remote-deploy.sh
@@ -19,12 +19,12 @@ function remote_deploy_metagraph() {
     deploy_dl1=false
   fi
 
-  if [ ! -z "$argc_deploy_only_metagraph_jars" ]; then
-    deploy_only_metagraph_jars=true
+  if [ ! -z "$argc_force_genesis" ]; then
+    force_genesis=true
   else
-    deploy_only_metagraph_jars=false
+    force_genesis=false
   fi
 
-  ansible-playbook -e "deploy_only_metagraph_jars=$deploy_only_metagraph_jars" -e "deploy_cl1=$deploy_cl1" -e "deploy_dl1=$deploy_dl1" -i $ANSIBLE_HOSTS_FILE $ANSIBLE_DEPLOY_PLAYBOOK_FILE
+  ansible-playbook -e "force_genesis=$force_genesis" -e "deploy_cl1=$deploy_cl1" -e "deploy_dl1=$deploy_dl1" -i $ANSIBLE_HOSTS_FILE $ANSIBLE_DEPLOY_PLAYBOOK_FILE
   echo_white "####################################################################"
 }
