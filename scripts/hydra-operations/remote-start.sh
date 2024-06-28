4df877327441336bc490b9204b36ad5b93b90efc
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2024-06-28 18:13:28 -0300

    Adding additional validations

--- scripts/hydra-operations/remote-start.sh
+++ scripts/hydra-operations/remote-start.sh
@@ -34,18 +34,35 @@ function remote_start_metagraph() {
   if [ -z "$argc_staking_parent_ordinal" ]; then
     staking_parent_ordinal=0
   fi
+
+  # Validate owner parameters
+  if [ "$force_owner_message" = "true" ]; then
+    if [ -z "$argc_owner_p12_file_name" ] || [ -z "$argc_owner_p12_alias" ] || [ -z "$argc_owner_p12_password" ]; then
+      echo "Error: When force_owner_message is set, you must provide owner_p12_file_name, owner_p12_alias, and owner_p12_password."
+      exit 1
+    fi
+  fi
+
+  # Validate staking parameters
+  if [ "$force_staking_message" = "true" ]; then
+    if [ -z "$argc_staking_p12_file_name" ] || [ -z "$argc_staking_p12_alias" ] || [ -z "$argc_staking_p12_password" ]; then
+      echo "Error: When force_staking_message is set, you must provide staking_p12_file_name, staking_p12_alias, and staking_p12_password."
+      exit 1
+    fi
+  fi
+
   ansible-playbook \
-  -e "force_genesis=$force_genesis" \
-  -e "owner_p12_file_name=$argc_owner_p12_file_name" \
-  -e "owner_p12_alias=$argc_owner_p12_alias" \
-  -e "owner_p12_password=$argc_owner_p12_password" \
-  -e "owner_parent_ordinal=$argc_owner_p12_password" \
-  -e "staking_p12_file_name=$argc_staking_p12_file_name" \
-  -e "staking_p12_alias=$argc_staking_p12_alias" \
-  -e "staking_p12_password=$argc_staking_p12_password" \
-  -e "force_owner_message=$force_owner_message" \
-  -e "owner_parent_ordinal=$owner_parent_ordinal" \
-  -e "force_staking_message=$force_staking_message" \
-  -e "staking_parent_ordinal=$staking_parent_ordinal" \
-  -i $ANSIBLE_HOSTS_FILE $ANSIBLE_NODES_START_PLAYBOOK_FILE
+    -e "force_genesis=$force_genesis" \
+    -e "owner_p12_file_name=$argc_owner_p12_file_name" \
+    -e "owner_p12_alias=$argc_owner_p12_alias" \
+    -e "owner_p12_password=$argc_owner_p12_password" \
+    -e "owner_parent_ordinal=$argc_owner_p12_password" \
+    -e "staking_p12_file_name=$argc_staking_p12_file_name" \
+    -e "staking_p12_alias=$argc_staking_p12_alias" \
+    -e "staking_p12_password=$argc_staking_p12_password" \
+    -e "force_owner_message=$force_owner_message" \
+    -e "owner_parent_ordinal=$owner_parent_ordinal" \
+    -e "force_staking_message=$force_staking_message" \
+    -e "staking_parent_ordinal=$staking_parent_ordinal" \
+    -i $ANSIBLE_HOSTS_FILE $ANSIBLE_NODES_START_PLAYBOOK_FILE
 }
