db7442b24c8d84d87e1e22c1296a785f00e3c053
Nicolas Meessen <nickmeessen@users.noreply.github.com>
2025-12-03 10:35:56 +0700

    fix ansible playbooks for ansible 2.20

--- scripts/hydra-operations/destroy.sh
+++ scripts/hydra-operations/destroy.sh
@@ -2,12 +2,10 @@
 function destroy_containers() {
     echo_title "################################## DESTROY ##################################"
     echo_white "Starting destroying containers ..."
-    
+
     export ANSIBLE_LOCALHOST_WARNING=False
     export ANSIBLE_INVENTORY_UNPARSED_WARNING=False
-    
-    NODES_JSON=$(echo "$NODES" | jq -c '.')
-    
-    ansible-playbook -e "nodes=${NODES_JSON}" -e "infra_path=${INFRA_PATH}" $ANSIBLE_LOCAL_CONTAINERS_DESTROY_PLAYBOOK_FILE
-    
+
+    ansible-playbook $ANSIBLE_LOCAL_CONTAINERS_DESTROY_PLAYBOOK_FILE
+
 }
